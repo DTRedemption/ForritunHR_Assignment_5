90dc9032f2819082eefec91aea32b8f5e9aa2fdc
Haraldur <haraldurloki@gmail.com>
2018-09-04 11:53:34 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,11 @@
+n = int(input("Enter the length of the sequence: "))  # Do not change this line
+
+# insert number "n"
+# use while loop
+# define a,b,c
+# a b c are 0 1 0 for this specific fibonacci sequence
+# while x less than number
+# create trib variable that is sum of a + b + c
+# assign a,b,c to trib, a and b respectively
+# increment by 1
+# print each number successively
